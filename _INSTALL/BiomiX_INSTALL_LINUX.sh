d0405da6d341f6f915217373c0f839143fa41708
IxI-97 <123497004+IxI-97@users.noreply.github.com>
2025-04-04 10:23:08 +0200

    Update BiomiX_INSTALL_LINUX.sh

--- _INSTALL/BiomiX_INSTALL_LINUX.sh
+++ _INSTALL/BiomiX_INSTALL_LINUX.sh
@@ -9,6 +9,7 @@ conda activate BiomiX-env
 conda config --add channels conda-forge
 conda install mamba=2.0.5 -y
 mamba install -c conda-forge r-base=4.4.1 -y
+mamba install -c conda-forge r-systemfonts=1.1.0 -y
 mamba install -c conda-forge r-rcpp=1.0.13 -y
 mamba install -c conda-forge r-systemfonts=1.1.0 -y
 python3 BiomiX_INSTALL_LINUX.py
